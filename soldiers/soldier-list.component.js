b0ce14d882f1bf5c88cd9f5e41b609604e4aa05f
Swierszcz <jakubborys1@gmail.com>
2017-12-07 14:37:27 +0100

    Poprawiony wygląd listy żołnierzy

--- soldiers/soldier-list.component.js
+++ soldiers/soldier-list.component.js
@@ -5,27 +5,35 @@ angular.
 	
     controller: function SoldierListController() {
       this.soldiers = [
-        {
+          {
 		  surname: 'Jakub',
           name: 'Sierżęga',
           age: '22',
+          rank: 'st. szer.',
+          function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         }, 
 		{
 		  surname: 'Jakub',
           name: 'Zając',
           age: '23',
+            rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         }, 
 		{
 		  surname: 'Marcin',
           name: 'Panek',
           age: '24',
+            rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Leszek',
           name: 'Platek',
+            rank: 'st. szer.',
+            function: 'działonowy-operator',
           age: '222',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
@@ -33,45 +41,53 @@ angular.
 		  surname: 'Maciej',
           name: 'Muras',
           age: '24',
+            rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Godwin',
           name: 'Edwin',
-          age: '28',
+          age: '28',          rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Sebastian',
           name: 'E. Paciorek',
-          age: '8',
+          age: '8',          rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Witali',
           name: 'Klieczko',
-          age: '45',
+          age: '45',          rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Hokus',
           name: 'Maryba',
-          age: '24',
+          age: '24',          rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Edek',
           name: 'Zgredek',
-          age: '25',
+          age: '25',          rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         },
 		{
 		  surname: 'Kapitan',
           name: 'Morgan',
-          age: '999',
+          age: '999',          rank: 'st. szer.',
+            function: 'działonowy-operator',
 		  imageUrl: 'img\\soldiers\\soldier_portrait.JPG'
         }
-		
       ];
+
     }
   });
